814bb9d7a743d7a98e6076f339223dfa1a03232e
Mitch <kinne174@umn.edu>
2019-08-11 00:42:03 -0500

    updated planning

---
+++ initial_dataset.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+# grab files with route labels from previous go through
+
+# grab old plays file with information on whether the play was a pass/run/sack
+# still not dealing with sacks since not worried about play success anymore, should be enough data points without it
+
+# for plays that were runs get receiver route information and label them run routes
+
+# combine run routes and pass routes
+
+# create one database with column headers from thoughts.py
+
+# create another database with all of the x y timestep points of the routes using the game Id, play Id, player iD info
+# from above dataset
