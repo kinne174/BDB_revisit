6eb9f650ba86185457fd709416c50141c954f76a
Mitch <kinne174@umn.edu>
2019-09-04 13:49:57 -0500

    after individual coverage completed move onto team coverage

---
+++ team_coverage.R
@@ -0,0 +1,25 @@
+# team coverage
+
+# load in same lineStrings as individual_coverage_active_learning.R
+# will just work with defense coverage guys and route runners
+# on each picture I want name and position of defenders, name, position and route of offensive players, 
+# maybe in white boxes to not be as cluttered
+
+# load in individual coverages
+
+# give specific gameId and playId
+
+# parse datasets using dplyr to make sure only looking at current play
+
+# get rows of defensive players in zone coverage
+
+# classify if they are deep/ middle/ close zone based on median depth from los
+
+# classify coverage # based on where safeties and cornerbacks are/ number of each in zone
+
+# depict where zone players should be based on coverage # and webisites such as:
+# https://thecollegefootballgirl.com/football-terms/advanced/coverage-schemes/
+
+# draw man connecting line to closest receiver on average throughout play, blitz arrow, and zone where
+# players should be with a line connecting player to center of zone.
+
